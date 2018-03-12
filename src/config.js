e5348302c564a34f971a14d19bfba2051e2d9c13
Ville Koivunen <ville.koivunen@keskiespoo.net>
2018-03-12 15:54:51 +0200

    Move to using shared config for deployer & UI for plugin locations

--- src/config.js
+++ src/config.js
@@ -1,7 +1,11 @@
+import { pluginMap, jotainihanmuuta } from './shared_config.json'
+
 const config = {
+  pluginMap: pluginMap,
   languages: ['fi', 'sv', 'en'],
   activeLanguage: 'fi',
   apiBaseUrl: (typeof window !== 'undefined' ? window.API_BASE_URL : null) || 'http://localhost:8000/',
   uiConfig: typeof window !== 'undefined' ? window.UI_CONFIG : null,
 };
+
 export default config;
