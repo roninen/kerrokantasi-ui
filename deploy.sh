bfdfb2c54c431d695604e8d49dc8efeaad31f77a
roninen <osoite@eitoimi.fi>
2018-04-17 14:44:44 +0300

    Trying more tagging

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,28 @@
 #!/usr/bin/env bash
 
-docker tag kerrokantasi-ui roninen/kerrokantasi-ui:latest
-docker push roninen/kerrokantasi-ui:latest
+# Skip PR deploys for now
+if [ "$TRAVIS_PULL_REQUEST" == true ]; then
+    echo "Skip PR deploy"
+    exit 0
+fi
+
+export REPO="roninen/kerrokantasi-ui"
+
+# Escape slashes in branch names
+export BRANCH=${TRAVIS_BRANCH//\/_}
+
+if [ "$TRAVIS_BRANCH" == "master" ] ; then
+    echo "Tagging master"
+    docker tag kerrokantasi-ui "$REPO:$COMMIT"
+    docker tag "$REPO:$COMMIT" $REPO:latest
+    docker push "$REPO:$COMMIT"
+fi
+
+if [ "$TRAVIS_BRANCH" != "master" ] ; then
+    echo "Tagging branch " "$TRAVIS_BRANCH"
+    docker tag kerrokantasi-ui "$REPO:$COMMIT"
+    docker tag "$REPO:$COMMIT" "$REPO:$BRANCH"
+    docker tag "$REPO:$COMMIT" "$REPO:travis-$TRAVIS_BUILD_NUMBER"
+    docker push "$REPO:$COMMIT"
+fi
+
